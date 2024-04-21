9ac85c8246788db796b53d9ccc0e7e00ee2143b2
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 04:21:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+They think of something you could store in a TypeScript Object. Write a program that creates
+Objects containing these items.
+*/
+// object is consist of key + value
+var stdDetail = {
+    name: "Ali",
+    // name key + ali value
+    age: 18,
+    gender: "male",
+    rollNo: 12345
+};
+console.log(stdDetail);
